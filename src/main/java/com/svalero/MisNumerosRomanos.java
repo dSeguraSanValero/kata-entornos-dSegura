a617952d36d3ab019417130fc99b2b9ed4965f47
Daniel-Segura <a27238@svalero.com>
2023-04-18 21:54:14 +0200

    Creados más tests. Implementada una lógica temporal en el método.
    Se han creado el resto de los tests (comprobación de los números 1, 7, 10, 134 y 2541). También se ha implementado una lógica temporal en el método a testear, basada en etiquetas de if else. Este método consigue superar los tres primeros test (números 1, 7 y 10), pero no es válido para convertir números grandes, ya que implica escribir todos los números tanto decimales como romanos hasta 3000.

--- src/main/java/com/svalero/MisNumerosRomanos.java
+++ src/main/java/com/svalero/MisNumerosRomanos.java
@@ -3,6 +3,30 @@ package com.svalero;
 
 public class MisNumerosRomanos {
     public static String convertirNumeroRomano(int numeroDecimal) {
-        return null;
+        String numeroRomano = null;
+        
+        if (numeroDecimal == 1) {
+            numeroRomano = "I";
+
+        } else if (numeroDecimal == 2) {
+            numeroRomano = "II";
+
+        } else if (numeroDecimal == 3) {
+            numeroRomano = "III";
+
+        } else if (numeroDecimal == 4) {
+            numeroRomano = "IV";
+
+        } else if (numeroDecimal == 5) {
+            numeroRomano = "V";
+
+        } else if (numeroDecimal == 6) {
+            numeroRomano = "VI";
+
+        } else if (numeroDecimal == 7) {
+            numeroRomano = "VII";
+
+        }
+        return numeroRomano;
     }
 }
--- src/test/java/com/svalero/MisNumerosRomanosTest.java
+++ src/test/java/com/svalero/MisNumerosRomanosTest.java
@@ -10,4 +10,24 @@ public class MisNumerosRomanosTest {
     public void testConversion1() {
         assertEquals("I", MisNumerosRomanos.convertirNumeroRomano(1));
     }
+
+    @Test
+    public void testConversion7() {
+        assertEquals("VII", MisNumerosRomanos.convertirNumeroRomano(7));
+    }
+
+    @Test
+    public void testConversion10() {
+        assertEquals("X", MisNumerosRomanos.convertirNumeroRomano(10));
+    }
+
+    @Test
+    public void testConversion134() {
+        assertEquals("CXXXIV", MisNumerosRomanos.convertirNumeroRomano(134));
+    }
+
+    @Test
+    public void testConversion2541() {
+        assertEquals("MMDXLI", MisNumerosRomanos.convertirNumeroRomano(2541));
+    }
 }
